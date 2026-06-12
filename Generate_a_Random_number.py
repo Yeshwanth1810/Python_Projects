a815af81e7fc275c47f283d1d5feff288fda26eb
Yeshwanth1810 <kolli.yeshwanth206@gmail.com>
2026-06-12 12:25:37 +0530

    Random_number_Generator

--- Generate_a_Random_number.py
+++ Generate_a_Random_number.py
@@ -1,3 +1,21 @@
 import random 
-lower_number=int(input("Enter the Lower Bound"))
-Upper_bound=int(input("Enter the Upper Bound"))
+print("Enter the Lower and Upper Value")
+lower_number=int(input("Enter the Lower Bound : "))
+Upper_bound=int(input("Enter the Upper Bound : "))
+num=random.randint(lower_number,Upper_bound)
+ch=7
+count=0
+print("You've 7 Chances to Predict the Correct Number")
+while count<ch:
+    count+=1
+    guess=int(input("Enter the Number : "))
+    if guess==num:
+        print(f"You've Guessed the Correct Number in {count} Chances ")
+        break
+    elif count>=ch and num!=guess:
+        print("You've Completed your chances,Better Luch nextTime !")
+    elif guess>num:
+        print("Too High")
+    elif guess<num:
+        print("Too low")
+        
